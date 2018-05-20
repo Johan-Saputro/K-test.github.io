e25ed473390a7fbb4c3e29334eec0f9fdd7043aa
Johan-Saputro <johansaputra474@gmail.com>
2018-05-20 14:00:26 +0700

    fhfhf
    fhhf

---
+++ K-test/js/mod.js
@@ -0,0 +1,24 @@
+function load() {
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('app').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
+    alert('Seamat Datang di Official Site dari K-Test')
+} 
+
+function home() {
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('app').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
+
+} 
+function app() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('app').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
+
+} 
+function about() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('app').style.display = 'none';
+    document.getElementById('about').style.display = 'block';
+} 
